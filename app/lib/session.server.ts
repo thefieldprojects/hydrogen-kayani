867fa5a32636cf20477b6dd9ecceb2c87ba39d74
Francis Regala <pantet008@gmail.com>
2023-05-07 22:26:05 +0800

    first commit

---
+++ app/lib/session.server.ts
@@ -0,0 +1,111 @@
+import {
+  createCookieSessionStorageFactory,
+  createCookieFactory,
+  type SessionStorage,
+  type Session,
+} from '@remix-run/server-runtime';
+
+import type {SignFunction, UnsignFunction} from '@remix-run/server-runtime';
+
+const encoder = new TextEncoder();
+
+export const sign: SignFunction = async (value, secret) => {
+  const data = encoder.encode(value);
+  const key = await createKey(secret, ['sign']);
+  const signature = await crypto.subtle.sign('HMAC', key, data);
+  const hash = btoa(String.fromCharCode(...new Uint8Array(signature))).replace(
+    /=+$/,
+    '',
+  );
+
+  return value + '.' + hash;
+};
+
+export const unsign: UnsignFunction = async (cookie, secret) => {
+  const value = cookie.slice(0, cookie.lastIndexOf('.'));
+  const hash = cookie.slice(cookie.lastIndexOf('.') + 1);
+
+  const data = encoder.encode(value);
+  const key = await createKey(secret, ['verify']);
+  const signature = byteStringToUint8Array(atob(hash));
+  const valid = await crypto.subtle.verify('HMAC', key, signature, data);
+
+  return valid ? value : false;
+};
+
+async function createKey(secret: string, usages: CryptoKey['usages']): Promise {
+  const key = await crypto.subtle.importKey(
+    'raw',
+    encoder.encode(secret),
+    {name: 'HMAC', hash: 'SHA-256'},
+    false,
+    usages,
+  );
+
+  return key;
+}
+
+function byteStringToUint8Array(byteString: string): Uint8Array {
+  const array = new Uint8Array(byteString.length);
+
+  for (let i = 0; i < byteString.length; i++) {
+    array[i] = byteString.charCodeAt(i);
+  }
+
+  return array;
+}
+
+/**
+ * This is a custom session implementation for your Hydrogen shop.
+ * Feel free to customize it to your needs, add helper methods, or
+ * swap out the cookie-based implementation with something else!
+ */
+export class HydrogenSession {
+  constructor(
+    private sessionStorage: SessionStorage,
+    private session: Session,
+  ) {}
+
+  static async init(request: Request, secrets: string[]) {
+    const createCookie = createCookieFactory({sign, unsign});
+    const createCookieSessionStorage =
+      createCookieSessionStorageFactory(createCookie);
+    const storage = createCookieSessionStorage({
+      cookie: {
+        name: 'session',
+        httpOnly: true,
+        path: '/',
+        sameSite: 'lax',
+        secrets,
+      },
+    });
+
+    const session = await storage.getSession(request.headers.get('Cookie'));
+
+    return new this(storage, session);
+  }
+
+  get(key: string) {
+    return this.session.get(key);
+  }
+
+  destroy() {
+    return this.sessionStorage.destroySession(this.session);
+  }
+
+  flash(key: string, value: any) {
+    this.session.flash(key, value);
+  }
+
+  unset(key: string) {
+    this.session.unset(key);
+  }
+
+  set(key: string, value: any) {
+    this.session.set(key, value);
+  }
+
+  commit() {
+    return this.sessionStorage.commitSession(this.session);
+  }
+}
---
+++ remix.env.d.ts
@@ -0,0 +1,35 @@
+import type {Storefront} from '~/lib/type';
+import type {HydrogenSession} from '~/lib/session.server';
+
+declare global {
+  /**
+   * A global `process` object is only available during build to access NODE_ENV.
+   */
+  const process: {env: {NODE_ENV: 'production' | 'development'} & Env};
+
+  /**
+   * Declare expected Env parameter in fetch handler.
+   */
+  interface Env {
+    SESSION_SECRET: string;
+    PUBLIC_STOREFRONT_API_TOKEN: string;
+    PRIVATE_STOREFRONT_API_TOKEN: string;
+    PUBLIC_STOREFRONT_API_VERSION: string;
+    PUBLIC_STORE_DOMAIN: string;
+  }
+}
+
+/**
+ * Declare local additions to `AppLoadContext` to include the session utilities we injected in `server.ts`.
+ */
+declare module '@shopify/remix-oxygen' {
+  export interface AppLoadContext {
+    waitUntil: ExecutionContext['waitUntil'];
+    session: HydrogenSession;
+    storefront: Storefront;
+    env: Env;
+  }
+}
+
+// Needed to make this file a module.
+export {}
